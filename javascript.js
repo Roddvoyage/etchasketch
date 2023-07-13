e4e3d7c930c7a01ab3bb20253ae1a65736ab6025
Roddvoyage <joao.f.rodrigues.93@gmail.com>
2023-07-13 21:31:46 +0100

    firstStages

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const grid = document.getElementById('grid');
+
+function makeGrid() {
+    let tb1 = document.getElementById('pixelCanvas');
+
+    for(let i=0; i < 11; i++){
+        let myRow = document.createElement('tr');
+        myRow.id = 'row' + i;
+
+        tb1.appendChild(myRow);
+        let rowW= document.getElementById('row' + i);
+    
+
+        for(let j = 0; j < 11; j++) {
+            let myCell = document.createElement('td');
+            rowW.appendChild(myCell);
+} 
+}
+}
+
